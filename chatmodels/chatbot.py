2e3f7978cd6ce1136254ec9c438aca04b9583c6c
Padala-Varun <varun2004varun2004@gmail.com>
2026-05-09 00:13:29 +0530

    Refactor chatbot message handling to use structured message types

--- chatmodels/chatbot.py
+++ chatmodels/chatbot.py
@@ -3,11 +3,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.messages import HumanMessage, AIMessage, SystemMessage
 
 model = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite",temperature=0.2,max_tokens=20)
 
 messages = [
-
+    SystemMessage(content="You are a funny chatbot.")
 ]
 
 print("-----------------------Welcome Type 0 to exit application-------------------------")
@@ -16,11 +17,11 @@ print("-----------------------Welcome Type 0 to exit application----------------
 while True: 
 
     prompt = input("You : ")
-    messages.append(prompt)
+    messages.append(HumanMessage(content=prompt))
     if prompt == "0":
         break
     response = model.invoke(messages)
-    messages.append(response.content)
+    messages.append(AIMessage(content=response.content))
 
     print("Bot = ",response.content)
 print("Messages:", messages)
